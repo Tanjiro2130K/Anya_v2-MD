ba805c19c6785a4abdf691076adede655ce33fc1
Tanjiro2130K <127303425+Tanjiro2130K@users.noreply.github.com>
2024-11-21 16:36:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,15 +18,15 @@ module.exports = {
      */
     ownername: process.env.Owner_Name || "TIKU✨🦋",
     ownernumber: process.env.Owner_Number || "917086726371",
-    instagramId: process.env.Insta || "3.69_pika",
+    instagramId: process.env.Insta || "_ne_itachi_",
 
     /**
      * other details and parameters
      */
-    author: process.env.Author || "𝐏𝐢𝐤𝐚𝐁𝐨𝐭𝐳 🌚🌙",
-    packname: process.env.PackName || "𝕼𝖚𝖊𝖊𝖓 𝕬𝖓𝖞𝖆 𝖁2 𝕾𝖙𝖎𝖈𝖐𝖊𝖗 𝕻𝖆𝖈𝖐 🪀",
-    socialLink: process.env.Web || "https://github.com/PikaBotz",
-    groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
+    author: process.env.Author || "TIKU✨🦋",
+    packname: process.env.PackName || "Love from your prince TIKU✨🦋",
+    socialLink: process.env.Web || "",
+    groupLink: process.env.GcLink || "https://chat.whatsapp.com/DQGWzesuDbz6Qq27pGyWlY",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
     mongoUrl: process.env.MongoDB || "mongodb+srv://tikuchan:TIKU12345@cluster0.5v1ps.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
@@ -49,14 +49,14 @@ module.exports = {
     ownercon: { key: { fromMe: false, participant: '0@s.whatsapp.net', ...({ remoteJid: 'status@broadcast' }), }, message: { contactMessage: { displayName: this.ownername, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:XL;${this.ownername},;;;\nFN:${this.ownername}\nitem1.TEL;waid=${this.ownernumber}:${this.ownernumber}\nitem1.X-ABLabel:Mobile\nEND:VCARD`, jpegThumbnail: this.image_2, thumbnail: this.image_2, sendEphemeral: true } } },
     fakeshop: { key: { fromMe: false, participant: "0@s.whatsapp.net", remoteJid: "status@broadcast" }, message: { orderMessage: { itemCount: 1234, status: 200, thumbnail: this.image_1, surface: 200, message: this.botname, orderTitle: this.ownername, sellerJid: '0@s.whatsapp.net'}}, contextInfo: { forwardingScore: 999, isForwarded: true}, sendEphemeral: true },
     message: {
-        success: "✅ 𝚂𝚞𝚌𝚌𝚎𝚜𝚜! 𝙾𝚙𝚛𝚊𝚝𝚒𝚘𝚗 𝙲𝚘𝚖𝚙𝚕𝚎𝚝𝚎𝚍.",
-        admin: "*👤 A𝙳𝙼𝙸𝙽 N𝙴𝙴𝙳𝙴𝙳!*\n\n- Dear, this command is only for Admins. You have to be a admin in this group to use this command.",
-        botAdmin: "*🤖 B𝙾𝚃 A𝙳𝙼𝙸𝙽 N𝙴𝙴𝙳𝙴𝙳!*\n\n- I'm not an Admin, so I can't execute this command in this group. Please make me an Admin.",
-        owner: "*👑 O𝚆𝙽𝙴𝚁 N𝙴𝙴𝙴𝙳𝙴𝙳!*\n\n- Bruh, this command is only made for this bot's owner. So you can't use this command.",
-        group: "*👥 G𝚛𝚘𝚞𝚙 N𝚎𝚎𝚍𝚎𝚍!*\n\n- This command can only be executed in a group chat.",
-        private: 'This command is only for private chats.',
-        wait: '🔄 Processing request...',
-        error: "❌ Oops! An error occurred while processing your request. Please try again later.",
+        success: "Its an wonderful complete 😊",
+        admin: "*hmm you are not an admin *\n\n- Dear, this command is only for Admins. You have to be a admin in this group to use this command ✌️",
+        botAdmin: "*give admin *\n\n- I'm not an Admin, so I can't execute this command in this group. Please make me an Admin 🌸",
+        owner: "*owner can only *\n\n- Bruh, this command is only made for this bot's owner. So you can't use this command 💐",
+        group: "*group*\n\n- This command can only be executed in a group chat 🍂",
+        private: 'This command is only for private chats 🪽',
+        wait: 'going on 👾',
+        error: "ahh 🥲 An error occurred while processing your request. Please try again later.",
         ban: `You're banned from using this bot!`,
         nsfw: 'This group is not *NSFW* enabled.',
         banChat: 'This group is banned from using this bot, please contact owner to get unbanned.'
